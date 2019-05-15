e35d4a72757685cd19c54b309db9be158c2f5f92
irenezyx <irenezyx@outlook.com>
2019-05-15 00:27:09 -0500

    memo

---
+++ client/main.js
@@ -0,0 +1,39 @@
+import { Meteor } from 'meteor/meteor';
+import ReactDOM from 'react-dom';
+import { Tracker } from 'meteor/tracker';
+import { Session } from 'meteor/session';
+import { createBrowserHistory } from 'history';
+const browserHistory = createBrowserHistory();
+import './main.html';
+
+import { routes, onAuthChange } from '../imports/routes/routes';
+import '../imports/startup/simple-schema-configuration.js';
+
+Tracker.autorun(() => {
+  const isAuthenticated = !!Meteor.userId();
+  const currentPagePrivacy = Session.get('currentPagePrivacy');
+
+  onAuthChange(isAuthenticated, currentPagePrivacy);
+});
+
+Tracker.autorun(() => {
+  const selectedNoteId = Session.get('selectedNoteId');
+  Session.set('isNavOpen', false);
+
+  if (selectedNoteId) {
+    browserHistory.replace(`/dashboard/${selectedNoteId}`);
+  }
+});
+
+Tracker.autorun(() => {
+  const isNavOpen = Session.get('isNavOpen');
+
+  document.body.classList.toggle('is-nav-open', isNavOpen);
+});
+
+Meteor.startup(() => {
+  Session.set('selectedNoteId', undefined);
+  Session.set('isNavOpen', false);
+  ReactDOM.render(routes, document.getElementById('app'));
+  console.log('main.js in client, route: ', route)
+});
---
+++ imports/ui/NoteListItem.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+import { Session } from 'meteor/session';
+import { createContainer } from 'meteor/react-meteor-data';
+
+export const NoteListItem = (props) => {
+  const className = props.note.selected ? 'item item--selected' : 'item';
+
+  return (
+    <div className={className} onClick={() => {
+      props.Session.set('selectedNoteId', props.note._id);
+    }}>
+      <h5 className="item__title">{ props.note.title || 'Untitled note' }</h5>
+      <p className="item__subtitle">{ moment(props.note.updatedAt).format('M/DD/YY') }</p>
+    </div>
+  );
+};
+
+NoteListItem.propTypes = {
+  note: PropTypes.object.isRequired,
+  Session: PropTypes.object.isRequired
+};
+
+export default createContainer(() => {
+  return { Session };
+}, NoteListItem);
